0e70702d227d683b76f3eba4b7fb081a99efcea1
Ben Petroski <ben@benpetroski.com>
2014-07-16 14:11:28 -0700

    Basic Framework - http://www.kilobolt.com/day-2-basic-framework

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -1,29 +1,48 @@
 package kiloboltgame;
 
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Event;
+import java.awt.Frame;
 import java.awt.Graphics;
 
-public class StartingClass extends Applet {
+public class StartingClass extends Applet implements Runnable {
 
+	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		super.init();
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
 	}
 
+	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		super.start();
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 
+	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		super.stop();
+
 	}
 
-	public void deliverEvent(Event arg0) {
+	@Override
+	public void destroy() {
 		// TODO Auto-generated method stub
-		super.deliverEvent(arg0);
+
 	}
 
+	public void run() {
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
