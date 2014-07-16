d8ffb40112c57f6500048c7bb8c89ecd83842d14
Ben Petroski <ben@benpetroski.com>
2014-07-16 14:44:50 -0700

    Taking user input -> http://www.kilobolt.com/day-3-taking-user-input

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -5,16 +5,19 @@ import java.awt.Color;
 import java.awt.Event;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void init() {
 		setSize(800, 480);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
-		frame.setTitle("Q-Bot Alpha");
+		frame.setTitle("Q-Bot Alpha");		
 	}
 
 	@Override
@@ -45,4 +48,51 @@ public class StartingClass extends Applet implements Runnable {
 			}
 		}
 	}
+
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Move left");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Move right");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			break;
+		}		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop moving left");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop moving right");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
+			break;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
