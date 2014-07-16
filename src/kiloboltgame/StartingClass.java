833dfb08f47ce13996be30ac71573e32bbabbc59
Ben Petroski <ben@benpetroski.com>
2014-07-16 12:59:19 -0700

    Second commit. Trying to get this thing to work...

---
+++ src/kiloboltgame/StartingClass.java
@@ -0,0 +1,29 @@
+package kiloboltgame;
+
+import java.applet.Applet;
+import java.awt.Event;
+import java.awt.Graphics;
+
+public class StartingClass extends Applet {
+
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	public void deliverEvent(Event arg0) {
+		// TODO Auto-generated method stub
+		super.deliverEvent(arg0);
+	}
+
+}
