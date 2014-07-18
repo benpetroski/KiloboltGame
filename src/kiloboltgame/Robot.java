b508c3c23076d356125698a6a342604d9e15767e
benpetroski <ben@benpetroski.com>
2014-07-17 23:46:22 -0700

    Finished adding enemies. There was a bug in mine, but I couldn't find it in my source, so I just used the given source -> http://www.kilobolt.com/day-5-background-and-sprites

--- src/kiloboltgame/Robot.java
+++ src/kiloboltgame/Robot.java
@@ -1,123 +1,91 @@
 package kiloboltgame;
 
+import java.awt.Graphics;
+
 public class Robot {
-	// Constants
-	final int JUMPSPEED = -15;
-	final int MOVESPEED = 5;
-	final int GROUND = 382;
-	
-	private int centerX = 100;
-	private int centerY = GROUND;
-	private boolean jumped = false;
-	private boolean movingLeft = false;
-	private boolean movingRight = false;
-	private boolean ducked = false;
-	private static Background bg1 = StartingClass.getBg1();
-	private static Background bg2 = StartingClass.getBg2();
-	private int speedX = 0;
-	private int speedY = 1;
 
-	public void update() {
+    // Constants are Here
+    final int JUMPSPEED = -15;
+    final int MOVESPEED = 5;
+    final int GROUND = 382;
+    
+    private int centerX = 100;
+    private int centerY = GROUND;
+    private boolean jumped = false;
+    private boolean movingLeft = false;
+    private boolean movingRight = false;
+    private boolean ducked = false;
+    private static Background bg1 = StartingClass.getBg1();                 
+    private static Background bg2 = StartingClass.getBg2();
+    private int speedX = 0;
+    private int speedY = 1;
 
-		// Moves character or scrolls background accordingly
-		
-		if (speedX < 0) {
+    public void update() {
+
+        // Moves Character or Scrolls Background accordingly.
+        if (speedX < 0) {
             centerX += speedX;
-        } if (speedX == 0 || speedX < 0) {
+        }
+        if (speedX == 0 || speedX < 0) {
             bg1.setSpeedX(0);
             bg2.setSpeedX(0);
-        } if (centerX <= 200 && speedX > 0) {
+
+        }
+        if (centerX <= 200 && speedX > 0) {
             centerX += speedX;
-        } if (speedX > 0 && centerX > 200){
+        }
+        if (speedX > 0 && centerX > 200){
             bg1.setSpeedX(-MOVESPEED);
             bg2.setSpeedX(-MOVESPEED);
-        } 
-        
-        // Updates y-position
+        }
+
+        // Updates Y Position
         centerY += speedY;
-		if (centerY + speedY >= GROUND) {
-			centerY = GROUND;
-		}
+        if (centerY + speedY >= GROUND) {
+            centerY = GROUND;
+        }
 
-		// Handles jumping
-		if (jumped == true) {
-			speedY += 1;
-			if (centerY + speedY >= GROUND) {
-				centerY = GROUND;
-				speedY = 0;
-				jumped = false;
-			}
-		}
+        // Handles Jumping
+        if (jumped == true) {
+            speedY += 1;
 
-		// Prevents going beyond x-position of 0
-		if (centerX + speedX <= 60) {
-			centerX = 61;
-		}
-	}
+            if (centerY + speedY >= GROUND) {
+                centerY = GROUND;
+                speedY = 0;
+                jumped = false;
+            }
 
-	public int getCenterX() {
-		return centerX;
-	}
+        }
 
-	public int getCenterY() {
-		return centerY;
-	}
+        // Prevents going beyond X coordinate of 0
+        if (centerX + speedX <= 60) {
+            centerX = 61;
+        }
+    }
 
-	public boolean isJumped() {
-		return jumped;
-	}
+    public void moveRight() {
+        if (ducked == false) {
+            speedX = MOVESPEED;
+        }
+    }
 
-	public int getSpeedX() {
-		return speedX;
-	}
+    public void moveLeft() {
+        if (ducked == false) {
+            speedX = -MOVESPEED;
+        }
+    }
 
-	public int getSpeedY() {
-		return speedY;
-	}
-
-	public void setCenterX(int centerX) {
-		this.centerX = centerX;
-	}
-
-	public void setCenterY(int centerY) {
-		this.centerY = centerY;
-	}
-
-	public void setJumped(boolean jumped) {
-		this.jumped = jumped;
-	}
-
-	public void setSpeedX(int speedX) {
-		this.speedX = speedX;
-	}
-
-	public void setSpeedY(int speedY) {
-		this.speedY = speedY;
-	}
-
-	public void moveRight() {
-		if (ducked == false) {
-			speedX = MOVESPEED;
-		}
-	}
-
-	public void moveLeft() {
-		if (ducked == false) {
-			speedX = MOVESPEED;
-		}
-	}
-
-	public void stopRight() {
+    public void stopRight() {
         setMovingRight(false);
         stop();
     }
 
     public void stopLeft() {
-        setMovingLeft(false);  
+        setMovingLeft(false);
         stop();
     }
-	
-    public void stop() {
+
+    private void stop() {
         if (isMovingRight() == false && isMovingLeft() == false) {
             speedX = 0;
         }
@@ -132,62 +100,76 @@ public class Robot {
 
     }
 
-	public void jump() {
-		if (jumped == false) {
-			speedY = JUMPSPEED;
-			jumped = true;
-		}
-	}
+    public void jump() {
+        if (jumped == false) {
+            speedY = JUMPSPEED;
+            jumped = true;
+        }
 
-	public int getJUMPSPEED() {
-		return JUMPSPEED;
-	}
+    }
 
-	public int getMOVESPEED() {
-		return MOVESPEED;
-	}
+    public int getCenterX() {
+        return centerX;
+    }
 
-	public int getGROUND() {
-		return GROUND;
-	}
+    public int getCenterY() {
+        return centerY;
+    }
 
-	public boolean isMovingLeft() {
-		return movingLeft;
-	}
+    public boolean isJumped() {
+        return jumped;
+    }
 
-	public boolean isMovingRight() {
-		return movingRight;
-	}
+    public int getSpeedX() {
+        return speedX;
+    }
 
-	public boolean isDucked() {
-		return ducked;
-	}
+    public int getSpeedY() {
+        return speedY;
+    }
 
-	public static Background getBg1() {
-		return bg1;
-	}
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
 
-	public static Background getBg2() {
-		return bg2;
-	}
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
 
-	public void setMovingLeft(boolean movingLeft) {
-		this.movingLeft = movingLeft;
-	}
+    public void setJumped(boolean jumped) {
+        this.jumped = jumped;
+    }
 
-	public void setMovingRight(boolean movingRight) {
-		this.movingRight = movingRight;
-	}
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
 
-	public void setDucked(boolean ducked) {
-		this.ducked = ducked;
-	}
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
 
-	public static void setBg1(Background bg1) {
-		Robot.bg1 = bg1;
-	}
+    public boolean isDucked() {
+        return ducked;
+    }
 
-	public static void setBg2(Background bg2) {
-		Robot.bg2 = bg2;
-	}
+    public void setDucked(boolean ducked) {
+        this.ducked = ducked;
+    }
+
+    public boolean isMovingRight() {
+        return movingRight;
+    }
+
+    public void setMovingRight(boolean movingRight) {
+        this.movingRight = movingRight;
+    }
+
+    public boolean isMovingLeft() {
+        return movingLeft;
+    }
+
+    public void setMovingLeft(boolean movingLeft) {
+        this.movingLeft = movingLeft;
+    }
+
 }
--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -10,144 +10,169 @@ import java.awt.event.KeyListener;
 import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
-	private Robot robot;
-	private Image image, character, background;
-	private URL base;
-	private Graphics second;
-	private static Background bg1, bg2;
 
-	@Override
-	public void init() {
-		setSize(800, 480);
-		setBackground(Color.BLACK);
-		setFocusable(true);
-		addKeyListener(this);
-		Frame frame = (Frame) this.getParent().getParent();
-		frame.setTitle("Q-Bot Alpha");
-		try {
-			base = getDocumentBase();
-		} catch (Exception e) {
-			// Handle expression
-		}
-		character = getImage(base, "data/character.png");
-		background = getImage(base, "data/background.png");
-	}
+    private Robot robot;
+    private Image image, currentSprite, character, characterDown, characterJumped, background;
+    private Graphics second;
+    private URL base;
+    private static Background bg1, bg2;
 
-	@Override
-	public void start() {
-		bg1 = new Background(0, 0);
-		bg2 = new Background(2160, 0);
-		robot = new Robot();
-		Thread thread = new Thread(this);
-		thread.start();
-	}
+    @Override
+    public void init() {
 
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
+        setSize(800, 480);
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
+        Frame frame = (Frame) this.getParent().getParent();
+        frame.setTitle("Q-Bot Alpha");
+        try {
+            base = getDocumentBase();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
-	}
+        // Image Setups
+        character = getImage(base, "data/character.png");
+        characterDown = getImage(base, "data/down.png");
+        characterJumped = getImage(base, "data/jumped.png");
+        currentSprite = character;
+        background = getImage(base, "data/background.png");
+    }
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
+    @Override
+    public void start() {
+        
+        bg1 = new Background(0,0);
+        bg2 = new Background(2160, 0);                robot = new Robot();
 
-	}
+        Thread thread = new Thread(this);
+        thread.start();
+    }
 
-	public void run() {
-		while (true) {
-			robot.update();
-			bg1.update();
-			bg2.update();
-			repaint();
-			try {
-				Thread.sleep(17);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	public void update(Graphics g) {
-		if (image == null) {
-			image = createImage(this.getWidth(), this.getHeight());
-			second = image.getGraphics();
-		}
-		second.setColor(getBackground());
-		second.fillRect(0, 0, getWidth(), getHeight());
-		second.setColor(getForeground());
-		paint(second);
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-		g.drawImage(image, 0, 0, this);
-	}
+    public void run() {
+        while (true) {
+            robot.update();
+            if (robot.isJumped()){
+                currentSprite = characterJumped;
+            }else if (robot.isJumped() == false && robot.isDucked() == false){
+                currentSprite = character;
+            }
+            bg1.update();
+            bg2.update();
+            repaint();
+            try {
+                Thread.sleep(17);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
-		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
-		g.drawImage(character, robot.getCenterX()-61, robot.getCenterY()-63, this);		
-	}
+    @Override
+    public void update(Graphics g) {
+        if (image == null) {
+            image = createImage(this.getWidth(), this.getHeight());
+            second = image.getGraphics();
+        }
 
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			System.out.println("Move up");
-			break;
-		case KeyEvent.VK_DOWN:
-			System.out.println("Move down");
-			break;
-		case KeyEvent.VK_LEFT:
-			robot.moveLeft();
-			break;
-		case KeyEvent.VK_RIGHT:
-			robot.moveRight();
-			break;
-		case KeyEvent.VK_SPACE:
-			robot.jump();
-			break;
-		}
-	}
+        second.setColor(getBackground());
+        second.fillRect(0, 0, getWidth(), getHeight());
+        second.setColor(getForeground());
+        paint(second);
 
-	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			System.out.println("Stop moving up");
-			break;
-		case KeyEvent.VK_DOWN:
-			System.out.println("Stop moving down");
-			break;
-		case KeyEvent.VK_LEFT:
-			robot.stop();
-			break;
-		case KeyEvent.VK_RIGHT:
-			robot.stop();
-			break;
-		case KeyEvent.VK_SPACE:
-			System.out.println("Stop jumping");
-			break;
-		}
-	}
+        g.drawImage(image, 0, 0, this);
 
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
+        g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
+        g.drawImage(currentSprite, robot.getCenterX() - 61, robot.getCenterY() - 63, this);
 
-	public static Background getBg1() {
-		return bg1;
-	}
+    }
 
-	public static void setBg1(Background bg1) {
-		StartingClass.bg1 = bg1;
-	}
+    public void keyPressed(KeyEvent e) {
 
-	public static Background getBg2() {
-		return bg2;
-	}
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            System.out.println("Move up");
+            break;
 
-	public static void setBg2(Background bg2) {
-		StartingClass.bg2 = bg2;
-	}
+        case KeyEvent.VK_DOWN:
+            currentSprite = characterDown;
+            if (robot.isJumped() == false){
+                robot.setDucked(true);
+                robot.setSpeedX(0);
+            }
+            break;
+
+        case KeyEvent.VK_LEFT:
+            robot.moveLeft();
+            robot.setMovingLeft(true);
+            break;
+
+        case KeyEvent.VK_RIGHT:
+            robot.moveRight();
+            robot.setMovingRight(true);
+            break;
+
+        case KeyEvent.VK_SPACE:
+            robot.jump();
+            break;
+
+        }
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            System.out.println("Stop moving up");
+            break;
+
+        case KeyEvent.VK_DOWN:
+            currentSprite = character;
+            robot.setDucked(false);
+            break;
+
+        case KeyEvent.VK_LEFT:
+            robot.stopLeft();
+            break;
+
+        case KeyEvent.VK_RIGHT:
+            robot.stopRight();
+            break;
+
+        case KeyEvent.VK_SPACE:
+            break;
+
+        }
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static Background getBg1() {
+        return bg1;
+    }
+
+    public static Background getBg2() {
+        return bg2;
+    }
 
 }
